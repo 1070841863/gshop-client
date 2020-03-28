1d8eaa254510b0241f2874070a618ccae692e9fc
1070841863 <1070841863@qq.com>
2020-03-28 15:05:28 +0800

    vue实战学习

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="msite">
+    <!--首页头部-->
+    <HeaderTop :title="address.name">
+       <router-link class="header_search" slot="left" to="search">
+            <i class="iconfont iconsousuo"></i>
+       </router-link>
+      <router-link class="header_login" slot="right" :to="userinfo._id?'/userinfo':'/login'">
+            <span class="header_login_text" v-if="!userinfo._id">登录|注册</span>
+            <span class="header_login_text" v-else><i class="iconfont icondaohanglan-05"></i></span>
+      </router-link>
+    </HeaderTop>
+    <!--首页导航-->
+    <nav class="msite_nav">
+      <div class="swiper-container" v-if="FoodTypes.length">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" v-for="(foodTypes,index) in FoodTypeArr" :key="index">
+            <a href="javascript:" class="link_to_food" v-for="(foodType,index) in foodTypes" :key="index">
+              <div class="food_container">
+                <img :src="baseImageUrl+foodType.image_url">
+              </div>
+              <span>{{foodType.title}}</span>
+            </a>
+          </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+      </div>
+      <img src="./images/msite_back.svg" alt="back" v-else>
+    </nav>
+    <!--首页附近商家-->
+    <div class="msite_shop_list">
+      <div class="shop_header">
+        <i class="iconfont icon-xuanxiang"></i>
+        <span class="shop_header_title">附近商家</span>
+      </div>
+      <ShopList></ShopList>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import {mapState} from 'vuex'
+  import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+  import ShopList from '../../components/ShopList/ShopList.vue'
+  import Swiper from 'swiper'
+  import 'swiper/css/swiper.css'
+  export default {
+    data(){
+      return{
+        baseImageUrl:"https://fuss10.elemecdn.com"
+      }
+    },
+    mounted(){
+      this.$store.dispatch('getFoodTypes'),
+      this.$store.dispatch('getshops')
+    },
+    computed:{
+      ...mapState(['address','FoodTypes','userinfo']),
+      /**
+       * 根据FoodTypes一维数组生成一个二维数组
+       * @constructor
+       */
+      FoodTypeArr(){
+        const {FoodTypes}=this
+        const  arr=[]
+        let foods=[]
+
+       FoodTypes.forEach(food=>{
+         //如果小数组里面的长度为8就将清空
+         if(foods.length===8){
+           foods=[]
+         }
+         //如果小数组里面的长度为0就将它放在大数组
+         if(foods.length===0){
+           arr.push(foods)
+         }
+         //往小数组里面放元素
+         foods.push(food)
+       })
+        return arr
+      }
+    },
+    watch:{
+      FoodTypes(value){  //FoodTypes数组有数据了
+
+        //界面更新就立即创建swiper对象
+        this.$nextTick(()=>{  //一旦完成界面更新，立即调用
+          new Swiper ('.swiper-container', {
+            loop: true,// 循环模式选项
+
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            }
+          })
+        })
+      }
+    },
+    components:{
+      HeaderTop,
+      ShopList
+    }
+  }
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .msite  //首页
+    width 100%
+    .msite_nav
+      bottom-border-1px(#e4e4e4)
+      margin-top 45px
+      height 200px
+      background #fff
+      .swiper-container
+        width 100%
+        height 100%
+        .swiper-wrapper
+          width 100%
+          height 100%
+          .swiper-slide
+            display flex
+            justify-content center
+            align-items flex-start
+            flex-wrap wrap
+            .link_to_food
+              width 25%
+              .food_container
+                display block
+                width 100%
+                text-align center
+                padding-bottom 10px
+                font-size 0
+                img
+                  display inline-block
+                  width 50px
+                  height 50px
+              span
+                display block
+                width 100%
+                text-align center
+                font-size 13px
+                color #666
+        .swiper-pagination
+          >span.swiper-pagination-bullet-active
+            background #02a774
+    .msite_shop_list
+      top-border-1px(#e4e4e4)
+      margin-top 10px
+      background #fff
+      .shop_header
+        padding 10px 10px 0
+        .shop_icon
+          margin-left 5px
+          color #999
+        .shop_header_title
+          color #999
+          font-size 14px
+          line-height 20px
+
+</style>
